c344cd216f2a9345faf658f1c59f5e0f554ebbc0
Delta Student <harshitmishra4181@gmail.com>
2025-10-11 11:52:15 +0530

    new

---
+++ src/seniorQuestion/builtInFunction.java
@@ -0,0 +1,18 @@
+package seniorQuestion;
+
+import java.util.Arrays;
+
+public class builtInFunction {
+    public static void main(String[] args){
+        int[] arr = {30,10,40,23,89,34};
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        //built in function
+        Arrays.sort(arr);
+        System.out.println();
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
