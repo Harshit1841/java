b3aac99ff0bc5ccd4cc89f6e4e4688afd3448de5
Delta Student <harshitmishra4181@gmail.com>
2025-10-13 20:02:45 +0530

    new

--- src/seniorQuestion/twoPointerInReverse.java
+++ src/seniorQuestion/twoPointerInReverse.java
@@ -2,13 +2,14 @@ package seniorQuestion;
 
 public class twoPointerInReverse {
     public static void main(String[]args){
-        int[] arr = {10,20,30,40,50};
+        int[] arr = {10,20,30,40,50,60,70};
         int n = arr.length;
         for(int ele: arr){
             System.out.print(ele+" ");
         }
         System.out.println();
         //two pointer approch
+        //int i=1, j=4; (part of array reverse);
         int i=0;
         int j=n-1;
         while(i<=j){
