4a8d4d9499e038d1f1763702a842026999dd98ab
Delta Student <harshitmishra4181@gmail.com>
2025-10-15 23:58:11 +0530

    new

---
+++ src/seniorQuestion/inputString.java
@@ -0,0 +1,12 @@
+package seniorQuestion;
+
+import java.util.Scanner;
+
+public class inputString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a profession: ");
+        String str = sc.nextLine();
+        System.out.print(str);
+    }
+}
