82d6af46721c0fdf5e8a46f5b20d855dc0b3c233
Delta Student <harshitmishra4181@gmail.com>
2025-10-15 23:45:25 +0530

    new

---
+++ src/seniorQuestion/whyString.java
@@ -0,0 +1,8 @@
+package seniorQuestion;
+
+public class whyString {
+    public static void main(String[] args){
+        String x= "Harshit Mishra is Software Developer.";
+        System.out.print(x);
+    }
+}
