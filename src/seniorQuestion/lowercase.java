43854883bec044bd3f5910e4e4d4b10886a1beef
Delta Student <harshitmishra4181@gmail.com>
2025-10-17 00:03:21 +0530

    new

--- src/seniorQuestion/lowercase.java
+++ src/seniorQuestion/lowercase.java
@@ -4,8 +4,16 @@ public class lowercase {
     public static void main(String[] args){
         String s = "Raghav Garg IS 24 Years Old ";
         //System.out.println(s.toLowerCase());
+        //s.toLowerCase(); -> nothing will happen
         s = s.toLowerCase();
         System.out.println(s);
         System.out.println(s.toUpperCase());
+
+        String a = "abc";
+        String b = "xyz";
+        System.out.println(a.concat(b));
+        System.out.println(a);
+        a = a.concat(b);
+        System.out.print(a);
     }
 }
