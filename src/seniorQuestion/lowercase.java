8971cb20bf33694d1b1ef9e580460c5e495acae5
Delta Student <harshitmishra4181@gmail.com>
2025-10-16 23:58:45 +0530

    new

---
+++ src/seniorQuestion/lowercase.java
@@ -0,0 +1,11 @@
+package seniorQuestion;
+
+public class lowercase {
+    public static void main(String[] args){
+        String s = "Raghav Garg IS 24 Years Old ";
+        //System.out.println(s.toLowerCase());
+        s = s.toLowerCase();
+        System.out.println(s);
+        System.out.println(s.toUpperCase());
+    }
+}
