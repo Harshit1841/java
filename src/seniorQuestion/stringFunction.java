bcb27ebf8cf565c6d21360c54d4750ad59362a38
Delta Student <harshitmishra4181@gmail.com>
2025-10-16 23:49:45 +0530

    new

---
+++ src/seniorQuestion/stringFunction.java
@@ -0,0 +1,21 @@
+package seniorQuestion;
+
+public class stringFunction {
+    public static void main(String[] args){
+        String s = "Raghav Garg";
+        System.out.println(s.indexOf('z'));
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.lastIndexOf('a'));
+
+        String a = "abc";
+        String b = "abcqqq";
+        System.out.println(a.compareTo(b));
+
+        String c = "Physics Wallah Skills";
+        System.out.println(c.contains("lla"));
+        System.out.println(c.startsWith("Phy"));
+        System.out.println(c.endsWith("lls"));
+
+        System.out.println();
+    }
+}
