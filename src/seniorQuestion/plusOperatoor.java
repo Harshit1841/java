3f8041221d616214a4b34a05bfb83d4fa8d3d785
Delta Student <harshitmishra4181@gmail.com>
2025-10-20 21:06:41 +0530

    new

--- src/seniorQuestion/plusOperatoor.java
+++ src/seniorQuestion/plusOperatoor.java
@@ -3,6 +3,7 @@ package seniorQuestion;
 public class plusOperatoor {
     public static void main(String[] args){
         String s = "abcde" +10;
+        // plus operater;
 //        s = s + 10;
         System.out.print(s);
 
