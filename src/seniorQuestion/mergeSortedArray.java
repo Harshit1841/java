eb1821c29a20edb0de8fce81727811ffd967713e
Delta Student <harshitmishra4181@gmail.com>
2025-10-23 22:50:16 +0530

    new

---
+++ src/seniorQuestion/mergeSortedArray.java
@@ -0,0 +1,35 @@
+public class mergeSortedArray {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8, 10};
+        int[] merged = new int[arr1.length + arr2.length];
+
+        int i = 0, j = 0, k = 0;
+
+        // Merge both arrays in sorted order
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] <= arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        // Copy remaining elements of arr1
+        while (i < arr1.length) {
+            merged[k++] = arr1[i++];
+        }
+
+        // Copy remaining elements of arr2
+        while (j < arr2.length) {
+            merged[k++] = arr2[j++];
+        }
+
+        // Print merged array
+        System.out.print("Merged Sorted Array: ");
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
