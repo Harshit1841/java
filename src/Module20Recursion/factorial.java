ebabc6b129b6369871764f4d490fa955cbf62ce8
Delta Student <harshitmishra4181@gmail.com>
2026-02-20 00:15:03 +0530

    new

---
+++ src/Module20Recursion/factorial.java
@@ -0,0 +1,16 @@
+package Module20Recursion;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static int fact(int n){
+        if(n==1) return 1;
+        return n * fact(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
