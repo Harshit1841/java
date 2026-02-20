c221d0d8dd6ff0c324b0134964a39b1e36b0aaec
Delta Student <harshitmishra4181@gmail.com>
2026-02-21 00:52:08 +0530

    new

--- src/Module20Recursion/printIncreasing.java
+++ src/Module20Recursion/printIncreasing.java
@@ -3,16 +3,16 @@ package Module20Recursion;
 import java.util.Scanner;
 
 public class printIncreasing {
-    static int n;
-    public static void print(int x){
+
+    public static void print(int x, int n){
         if(x>n) return;
         System.out.println(x);
-        print(x+1);
+        print(x+1,n);
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n : ");
-        n = sc.nextInt();
-        print(1);
+        int n = sc.nextInt();
+        print(1,n);
     }
 }
