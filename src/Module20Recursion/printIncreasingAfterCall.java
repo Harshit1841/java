ca9ee429695b8f0f406ad993fa67ecc47ac56013
Delta Student <harshitmishra4181@gmail.com>
2026-02-22 00:51:26 +0530

    new

---
+++ src/Module20Recursion/printIncreasingAfterCall.java
@@ -0,0 +1,18 @@
+package Module20Recursion;
+
+import java.util.Scanner;
+
+public class printIncreasingAfterCall {
+    public static void print(int n){
+        if(n==0) return; // base case
+        print(n-1);// call
+        System.out.println(n); //work
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        print(n);
+
+    }
+}
